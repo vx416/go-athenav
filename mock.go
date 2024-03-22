1c269fcacd105e6eee3233000de3f58e128300ac
vic <vic@psp-power.com.tw>
2024-03-22 14:37:19 +0800

    add mock query for athena mocking data

--- mock.go
+++ mock.go
@@ -1,8 +1,43 @@
 package athena
 
-import "github.com/aws/aws-sdk-go/service/athena/athenaiface"
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/athena"
+	"github.com/aws/aws-sdk-go/service/athena/athenaiface"
+	"github.com/stretchr/testify/mock"
+)
 
 // AthenaAPI is an interface that represents the AthenaAPI. It's useful for mocking.
 type AthenaAPI interface {
 	athenaiface.AthenaAPI
+}
+
+type Mocker interface {
+	On(methodName string, arguments ...interface{}) *mock.Call
+}
+
+// MockQuery mocks the AthenaAPI to return the given columns and data rows.
+func MockQuery(mocker Mocker, mockColumnsType []string, mockDataRows []*athena.Row) {
+	queryID := fmt.Sprintf("query-%d", time.Now().UnixNano())
+	state := athena.QueryExecutionStateSucceeded
+	mocker.On("StartQueryExecution", mock.Anything, mock.Anything).Return(&athena.StartQueryExecutionOutput{QueryExecutionId: &queryID}, nil)
+	mocker.On("GetQueryExecutionWithContext", mock.Anything, mock.Anything).Return(&athena.GetQueryExecutionOutput{QueryExecution: &athena.QueryExecution{Status: &athena.QueryExecutionStatus{
+		State: &state,
+	}}}, nil)
+	columnInfos := make([]*athena.ColumnInfo, len(mockColumnsType))
+	for i, colType := range mockColumnsType {
+		columnInfos[i] = &athena.ColumnInfo{
+			Type: &colType,
+		}
+	}
+	mocker.On("GetQueryResults", mock.Anything, mock.Anything).Return(&athena.GetQueryResultsOutput{
+		ResultSet: &athena.ResultSet{
+			ResultSetMetadata: &athena.ResultSetMetadata{
+				ColumnInfo: columnInfos,
+			},
+			Rows: mockDataRows,
+		},
+	}, nil)
 }
